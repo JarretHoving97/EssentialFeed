42b58abf3f75bc06d7b9dcf5e852f245c7242571
Jarret Hoving <jarret.hoving97@gmail.com>
2024-02-13 19:20:28 +0100

    Memoryleak tracking for components in integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -25,11 +25,13 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient() 
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
+        checkForMemoryLeaks(client, file: file, line: line)
+        checkForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: LoadFeedResult?
