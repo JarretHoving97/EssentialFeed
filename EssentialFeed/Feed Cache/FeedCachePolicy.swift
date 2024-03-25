2bd781044b87954e1515aae4cdb3ddec97dc313d
Jarret Hoving <jarret.hoving@wemamobile.nl>
2024-03-25 20:02:53 +0100

    Move 'FeedCachePolicy' to it's own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,28 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Jarret Hoving on 25/03/2024.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    
+    private init() {}
+    
+    static private let calendar = Calendar(identifier: .gregorian)
+    
+    static private var maxCacheDateInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheDateInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+    
+}
--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,26 +7,6 @@
 
 import Foundation
 
-public final class FeedCachePolicy {
-    
-    private init() {}
-    
-    static private let calendar = Calendar(identifier: .gregorian)
-    
-    static private var maxCacheDateInDays: Int {
-        return 7
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheDateInDays, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-    
-}
-
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
