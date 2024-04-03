0785dab64576f0ca1a75c73e415f58f58e96cf83
Jarret Hoving <jarret.hoving@wemamobile.nl>
2024-04-03 20:57:56 +0200

    retrieving from empty cache retrieves empty result

---
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodableFeedStoreTestss.swift
+//  EssentialFeedTests
+//
+//  Created by Jarret Hoving on 03/04/2024.
+//
+
+import XCTest
+import EssentialFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {}
+}
+
+
+final class CodableFeedStoreTestss: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break;
+                
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+    }
+
+}
