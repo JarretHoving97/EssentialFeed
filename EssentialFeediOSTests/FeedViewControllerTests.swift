b747189f3152d99d59dacc35d9a8423a5ce3c6e5
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-08-08 21:05:43 +0200

    Move FeedViewController to production target

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -8,58 +8,8 @@
 import XCTest
 import UIKit
 import EssentialFeed
+import EssentialFeediOS
 
-final class FeedViewController: UITableViewController {
-    
-    private var onViewIsAppearing: ((FeedViewController) -> Void)?
-    
-    var loader: FeedLoader?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        
-        onViewIsAppearing = { vc in
-            vc.onViewIsAppearing = nil
-            vc.load()
-        }
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        onViewIsAppearing?(self)
-    }
-
-    @objc private func load() {
-        
-        refreshControl?.beginRefreshing()
-        
-        loader?.load { [weak self] _ in
-            guard let self else { return }
-            self.refreshControl?.endRefreshing()
-        }
-    }
-}
-
-
-private extension FeedViewController {
-    
-    func simulateAppearance() {
-        if !isViewLoaded {
-            loadViewIfNeeded()
-            replaceRefreshControlWithFakeiOS17Support()
-        }
-        
-        beginAppearanceTransition(true, animated: false)
-        endAppearanceTransition()
-    }
-}
 
 final class FeedViewControllerTests: XCTestCase {
     
@@ -126,18 +76,6 @@ final class FeedViewControllerTests: XCTestCase {
     }
 }
 
-private extension FeedViewController {
-    
-    func simulateUserInitiatedFeedReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    var isShowingIndicator: Bool {
-        return refreshControl?.isRefreshing == true
-    }
-}
-
-
 private extension UIRefreshControl {
     
     func simulatePullToRefresh() {
@@ -147,5 +85,25 @@ private extension UIRefreshControl {
                 (target as NSObject).perform(Selector($0))
             }
         }
+    }
+}
+
+private extension FeedViewController {
+    
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            replaceRefreshControlWithFakeiOS17Support()
+        }
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    var isShowingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
     }
 }
