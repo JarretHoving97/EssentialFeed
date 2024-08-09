bc25da8b4a16ab713e01801791a3d57392ba7c36
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-08-09 10:53:33 +0200

    fixed type

--- EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class EssentialFeediOSTests: XCTestCase {
 
-    override func se tUpWithError() throws {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
