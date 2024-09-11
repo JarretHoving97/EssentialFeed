668d778905112d64dbe52f9e908ecdeed0aa7f19
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-11 20:07:28 +0200

    Updated `FeedImageCellController` to keep up with the course (probably missed a part)

--- EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -13,11 +13,10 @@ public final class FeedImageCell: UITableViewCell {
     public let descriptionLabel = UILabel()
     public let feedImageContainer = UIView()
     public let feedImageView = UIImageView()
-
+    
     private(set) public lazy var feedImageRetryButton: UIButton = {
         let button = UIButton()
         button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
-        
         return button
     }()
     
@@ -26,4 +25,5 @@ public final class FeedImageCell: UITableViewCell {
     @objc private func retryButtonTapped() {
         onRetry?()
     }
+
 }
