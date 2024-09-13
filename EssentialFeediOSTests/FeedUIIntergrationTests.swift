df0cac96d7e64dfd053f219fc79fa45924a6af95
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-13 16:45:02 +0200

    Rename `FeedViewControllerTests` to `FeedUIIntegrationTests` since we are testing the composition of multiple UI components in integration

--- EssentialFeediOSTests/FeedUIIntergrationTests.swift
+++ EssentialFeediOSTests/FeedUIIntergrationTests.swift
@@ -11,7 +11,7 @@ import EssentialFeed
 import EssentialFeediOS
 
 
-final class FeedViewControllerTests: XCTestCase {
+final class FeedUIIntergrationTests: XCTestCase {
     
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
@@ -19,9 +19,6 @@ final class FeedViewControllerTests: XCTestCase {
 
         XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
     }
-    
-
-
     
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut, loader) = makeSUT()
