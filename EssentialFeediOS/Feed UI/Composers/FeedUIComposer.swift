d9244f98409e33e2345c0b297bacaedda5234135
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-13 21:25:02 +0200

    make `MainTheadDispatchDecorator` generic

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -27,22 +27,26 @@ public final class FeedUIComposer {
     }
 }
 
-private final class MainTheadDispatchDecorator: FeedLoader {
+private final class MainTheadDispatchDecorator<T> {
     private let decoratee: FeedLoader
-    
     init(decoratee: FeedLoader) {
         self.decoratee = decoratee
     }
     
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { completion() }
+        }
+         
+        completion()
+    }
+}
+
+extension MainTheadDispatchDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        dispatch { [weak self] in
+            self?.decoratee.load(completion: completion)
         }
     }
 }
