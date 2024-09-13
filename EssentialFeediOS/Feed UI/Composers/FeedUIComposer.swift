f5852f46af609b076bce8a0e653666d893475499
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-13 21:35:35 +0200

    `MainTheadDispatchDecorator` to separate file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -27,38 +27,7 @@ public final class FeedUIComposer {
     }
 }
 
-private final class MainTheadDispatchDecorator<T> {
-    private let decoratee: T
 
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { completion() }
-        }
-         
-        completion()
-    }
-}
-
-extension MainTheadDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        dispatch { [weak self] in
-            self?.decoratee.load(completion: completion)
-        }
-    }
-}
-
-extension MainTheadDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
 
 private extension FeedViewController {
     
