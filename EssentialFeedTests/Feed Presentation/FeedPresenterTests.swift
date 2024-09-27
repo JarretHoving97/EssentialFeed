7b666b6b886466cefb8c737a24927005a2e053aa
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-27 16:35:05 +0200

    `FeedPresenter` presents no error on `didStartLoadingFeed`

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,28 @@
 
 import XCTest
 
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
     }
 }
 
@@ -17,19 +36,36 @@ class FeedPresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
-        
+  
         XCTAssert(view.messages.isEmpty)
+    }
+    
+    func test_didStartLoadingFeed_displayErrorMessage() {
+        let (sut, view) = makeSUT()
+        sut.didStartLoadingFeed()
+    
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
     }
     
     // MARK: Helpers
     
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedErrorView {
+        
+     
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+       private(set) var messages = [Message]()
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         checkForMemoryLeaks(view, file: file, line: line)
         checkForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
