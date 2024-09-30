f60af0c3b215d263763b126b67195db8e7e20cf4
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-09-30 20:54:26 +0200

    Extract duplicate HTTPClientSpy helper into a shared scope

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -177,39 +177,4 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         let json = ["items": items]
         return try! JSONSerialization.data(withJSONObject: json)
     }
-    
-    // client
-    private class HTTPClientSpy: HTTPClient {
-        
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-
-        private var messages: [(url: URL, completion: (HTTPClient.Result) -> Void)] = []
-        
-        var requestedURLs: [URL] {
-            return messages.map({$0.url})
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0 ) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(with statusCode: Int, data: Data, at index: Int = 0 ) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: statusCode,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            
-            // trigger the completion inside of messages
-            messages[index].completion(.success((data, response)))
-        }
-    }
 }
--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -88,7 +88,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: failure(.invalidData), when: {
-                client.complete(withStatusCode: code, data: anyData(), at: index)
+                client.complete(with: code, data: anyData(), at: index)
             })
         }
     }
@@ -98,7 +98,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         
         expect(sut, toCompleteWith: failure(.invalidData), when: {
             let emptyData = Data()
-            client.complete(withStatusCode: 200, data: emptyData)
+            client.complete(with: 200, data: emptyData)
         })
     }
     
@@ -110,13 +110,10 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         sut?.loadImageData(from: anyURL()) { capturedResults.append($0) }
         
         sut = nil
-        client.complete(withStatusCode: 200, data: anyData())
+        client.complete(with: 200, data: anyData())
         
         XCTAssertTrue(capturedResults.isEmpty)
     }
-
-
-
 
     // MARK: Helpers
     
@@ -164,40 +161,5 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
 
         
         wait(for: [exp], timeout: 1.0)
-    }
-
-
-    private class HTTPClientSpy: HTTPClient {
-        
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        private(set) var cancelledURLs = [URL]()
-
-        var requestedURLs: [URL] {
-            return messages.map { $0.url }
-        }
-
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success((data, response)))
-        }
-
     }
 }
