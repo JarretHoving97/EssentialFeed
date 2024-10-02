d0f5dbb5dd7a55d27538f936a98ddbaace35334e
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-10-02 21:14:22 +0200

    added empty app

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Jarret Hoving on 02/10/2024.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+}
