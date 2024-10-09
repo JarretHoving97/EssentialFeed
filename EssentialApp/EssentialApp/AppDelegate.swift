6767201f63b84a6b28e482f55b4777274e069098
Jarret Hoving <Jarret.hoving97@gmail.com>
2024-10-09 22:48:32 +0200

    Move DEBUG code paths from the main SceneDelegate to a new DebuggingSceneDelegate subclass to separate debug- and test-specific code from production code

--- EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -18,7 +18,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let sceneConfig: UISceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-        sceneConfig.delegateClass = SceneDelegate.self
+        #if DEBUG
+        sceneConfig.delegateClass = DebuggingSceneDelegate.self
+        #endif
         return sceneConfig
     }
 }
